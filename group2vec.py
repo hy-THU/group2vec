a1208843dbe564ada3410cb4e740dd99292ca1ae
yuhan <pla_hy@126.com>
2018-02-28 21:38:34 +0800

    update

--- group2vec.py
+++ group2vec.py
@@ -18,3 +18,48 @@ from tqdm import tqdm
 from multiprocessing import Pool
 import cPickle as pickle
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run CPNE.")
+    parser.add_argument('--edge_list', default='edges_week', help='Input edge list path.')
+    parser.add_argument('--group_members', default='pos_week', help='Input group members path.')
+    parser.add_argument('--num_threads', type=int, default=10, help='Number of threads.')
+    parser.add_argument('--num_walks', type=int, default=100, help='Number of walks for generating the group corpus.')
+    parser.add_argument('--num_walks_trans', type=int, default=100, help='Number of walks for generating the transition matrices with DeepWalk.')
+    parser.add_argument('--walk_length', type=int, default=100, help='Length of walks for generating the group corpus.')
+    parser.add_argument('--walk_length_trans', type=int, default=100, help='Length of walks for generating the transition matrices with DeepWalk.')
+    parser.add_argument('--neg_cnt', type=int, default=5, help='Number of negtive instances for generating the group corpus.')
+    parser.add_argument('--neg_cnt_trans', type=int, default=5, help='Number of negtive instances for generating the transition matrices with DeepWalk.')
+    parser.add_argument('--dimension', type=int, default=128, help='Number of dimensions for group embedding.')
+    parser.add_argument('--dimension_trans', type=int, default=128, help='Number of dimensions for node embedding used to generating the transition matrices.')
+
+    parser.add_argument('--learning_rate', type=float, default=0.1, help='Learning rate to learn group embedding.')
+
+    parser.add_argument('--weighted', dest='weighted', action='store_true',
+                        help='Boolean specifying (un)weighted. Default is unweighted.')
+    parser.add_argument('--unweighted', dest='unweighted', action='store_false')
+    parser.set_defaults(weighted=True)
+    parser.add_argument('--directed', dest='directed', action='store_true',
+                        help='Graph is (un)directed. Default is undirected.')
+    parser.add_argument('--undirected', dest='undirected', action='store_false')
+    parser.set_defaults(directed=False)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    # loading the data
+    f = open('G.dat', 'r')
+    G, groups, labels, _ = pickle.load(f)
+    f.close()
+    num_nodes = G.number_of_nodes()
+    num_groups = len(groups)
+
+    # generating the transition matrices
+    neighbors
+    weights
+
+    # generating group corpus
+
+    # embedding
